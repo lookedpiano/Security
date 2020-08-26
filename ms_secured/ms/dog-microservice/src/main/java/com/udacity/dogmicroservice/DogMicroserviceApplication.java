c714603ffb7b16613ddfb8a96171f94ca3e93379
lookedpiano <70062908+lookedpiano@users.noreply.github.com>
2020-08-26 15:01:21 +0200

    Add files via upload

---
+++ ms_secured/ms/dog-microservice/src/main/java/com/udacity/dogmicroservice/DogMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.udacity.dogmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DogMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogMicroserviceApplication.class, args);
+	}
+
+}
---
+++ ms_secured/ms/dog-microservice/target/classes/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO dawg (id, name, breed, origin) VALUES (1, 'Jeffrey','Dackel', 'DE');
+INSERT INTO dawg (id, name, breed, origin) VALUES (2, 'Bully','Dackel', 'US');
+INSERT INTO dawg (id, name, breed, origin) VALUES (4, 'x','y', null);
